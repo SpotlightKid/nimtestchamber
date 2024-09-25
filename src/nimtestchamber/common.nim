c40ab4516f0e1a96cf5ba873acd930198ea3be0e
Christopher Arndt <chris@chrisarndt.de>
2024-09-25 08:24:55 +0200

    feat: windows support
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- src/nimtestchamber/common.nim
+++ src/nimtestchamber/common.nim
@@ -1,10 +1,18 @@
 import std/[paths, posix]
 
 
+when defined(windows):
+    proc win_execv*(path: cstring, params: cstringArray): int64 {.importc: "_execv", header: "<process.h>", sideEffect.}
+
+
 proc exec*(path: string, params: seq[string]) =
+    var c_params = allocCStringArray(@[string(Path(path).splitFile().name)] & params)
+
     when defined(posix):
-        var c_params = allocCStringArray(@[string(Path(path).splitFile().name)] & params)
         discard execv(path.cstring, c_params)
-        deallocCStringArray(c_params)
+    elif defined(windows):
+        discard win_execv(path.cstring, c_params)
     else:
         raise newException(OSError, "OS not supported.")
+
+    deallocCStringArray(c_params)
--- tests/test.nim
+++ tests/test.nim
@@ -1,3 +1,14 @@
 ## Put your tests here.
 
-import nimtestchamber
+import std/[osproc, strformat, strutils, unittest]
+
+let cmd = when defined(windows):
+    "nimtestchamber.exe"
+else:
+    "nimtestchamber"
+
+test "Run nimbletestchamber command":
+    var (output, status) = execCmdEx(&"bin/{cmd} foo bar")
+    check status == 0
+    output.stripLineEnd()
+    check output == "foo bar"
